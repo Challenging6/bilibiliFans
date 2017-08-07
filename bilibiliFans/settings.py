a3cf22a1ef48f2370559252c197bc23946ae6dda
Challenging6 <992871471@qq.com>
2017-08-07 10:33:33 +0800

    add ip

--- bilibiliFans/settings.py
+++ bilibiliFans/settings.py
@@ -42,7 +42,6 @@ ROBOTSTXT_OBEY = False
 DEFAULT_REQUEST_HEADERS = {
 
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-
 }
 
 # Enable or disable spider middlewares
